ead148534c19def9feb0003b29bee2018be8e5f0
Sachin Sharma <sachinsharmame40@gmail.com>
2024-02-08 22:36:44 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    const[records,Setrecords]=useState([
+                    {roll:1000,name:'amit'},
+                    {roll:1001,name:'sumit'},
+                    {roll:1002,name:'rohit'},
+                    {roll:1003,name:'shyam'},
+                    {roll:1004,name:'raju'}
+    ])
+    const handleUpdate=()=>{
+    Setrecords([
+                    {roll:2000,name:'amit-g'},
+                    {roll:2001,name:'sumit-g'},
+                    {roll:2002,name:'rohit-g'},
+                    {roll:2003,name:'shyam-g'},
+                    {roll:2004,name:'raju-g'}
+    ])
+    }
+    return(
+        <div>
+        <h1>App Component</h1>
+        <input type="button" value="update data" onClick={handleUpdate} /> <br />
+        {
+            records.map(row=>{return(
+            <div key={row.roll} className="box">
+            <h2>{row.roll}</h2>
+            <p>{row.name}</p>
+            </div>
+            )})
+        }
+        </div>
+    )
 }
-
 export default App;
